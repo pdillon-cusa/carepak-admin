2795221fcc9ab1c40b657d01fc915bf94a17c2d3
Peter Dillon <q12721@corp.cusa.canon.com>
2018-06-01 10:31:36 -0400

    more work on promo maintenance screen

--- promotion-maintenance/js/promotion-maintenance.js
+++ promotion-maintenance/js/promotion-maintenance.js
@@ -1,22 +1,6 @@
 
 
 //-------------------------------------------------
-//---------------- Form Validation ----------------
-$().ready(function() {
-    var validator = $("#editPromotionInfo").validate({
-        debug: true,
-        rules: {
-            carePakType: {
-                required: true
-            }
-        },
-        submitHandler: function(form) {
-            closeModal();
-        }
-    });
-});
-
-//-------------------------------------------------
 //---------------- DataTable Rules ----------------
 $(document).ready(function() {
     $('#promotionMaintenanceData').DataTable({
@@ -25,6 +9,7 @@ $(document).ready(function() {
             { data: 'productSku' },
             { data: 'productName' },
             { data: 'productModel' },
+            { data: 'order' },
             { data: 'carePakSku' },
             { data: 'carePakType' },
             { data: 'carePakPeriod' },
@@ -44,12 +29,70 @@ $(document).ready(function() {
             orderable: false,
             order: []
         },
+        {
+            targets: [3],
+            orderable: true,
+            visible: false
+        },
         { 
             className: "dt-center", 
             targets: [4,5,6,8,9]
         },]
     });
-});
+
+    //-------------------------------------------------
+    //----------- Add Promotion Validation ------------
+    var validateAddPromo = $("#addPromotionInfo").validate({
+        debug: true,
+        rules: {
+            carePakType: {
+                required: true
+            }
+        },
+        submitHandler: function(form) {
+            addNewPromotionRow();
+            closeModal();
+        }
+    });
+
+    //-------------------------------------------------
+    // --------------- Add SKU Row --------------------
+    function addNewPromotionRow() {
+
+        var table = $('#promotionMaintenanceData').DataTable();
+        var rowNode = table.row.add({
+            "productName": newProductName.value,
+            "productSku": newProductSku.value,
+            "productModel": newProductModel.value,
+            "order": -1,
+            "carePakSku": newcpSku.value,
+            "carePakType": newcpType.value,
+            "carePakPeriod": newcpPeriod.value,
+            "startDate": newStartDate.value,
+            "endDate": newEndDate.value,
+            "registrationPeriod": newRegistrationPeriod.value,
+            "edit": '<a href="#" onclick="showAddPromotionModal(' + '\'' + `${newProductName.value}` + '\'' + ', ' + `${newProductSku.value}` + ', \'' + `${newProductModel.value}` + '\'' + ')">EDIT</a>',
+        }).draw(false).order([3, 'asc']).draw().nodes().to$().addClass('activated');
+        
+        // ------ Remove Class So Only New Row Animates ------------
+        var newRow = document.getElementsByClassName('activated')[0];
+        newRow.addEventListener("animationend", removeAnimation, false);
+        function removeAnimation() {
+            newRow.classList.remove('activated');
+          }
+    }
+
+    //-------------------------------------------------
+    // ----------- Init Date Pickers ------------------
+    $("#startDate").datepicker();
+    $("#newStartDate").datepicker('setDate', new Date());
+    $("#endDate").datepicker();
+    $("#newEndDate").datepicker('setDate', "+3m");
+
+    //---------------------------------------------
+}); //  --------- / doc.ready ---------------------
+    //---------------------------------------------
+
 
 //-------------------------------------------------
 // --------- Edit Promotion Dialog ----------------
@@ -69,15 +112,15 @@ function showEditPromotionModal(productSku, productName, productModel, carePakSk
     // Show the overlay
     $(".overlay").css('display', 'block');
     $("#editPromotionModal").css('display', 'block');
-    $("#editPromotionInfo").data('validator').resetForm();
+   // $("#editPromotionInfo").data('validator').resetForm();
 }
 
 //-------------------------------------------------
-// ------------- Add SKU Dialog -------------------
+// ------------- Add Promotion Dialog -------------------
 function showAddPromotionModal() {
     $('.custom-modal__title').text('Add Promotion');
     // Show the overlay
     $(".overlay").css('display', 'block');
-    $("#editPromotionModal").css('display', 'block');
-    $("#editPromotionInfo").data('validator').resetForm();
+    $("#addPromotionModal").css('display', 'block');
+   // $("#editPromotionInfo").data('validator').resetForm();
 }
