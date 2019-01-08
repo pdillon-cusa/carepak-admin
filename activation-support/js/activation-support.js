62499f3d8dedac3b9f39c70da04b4eee323589e0
Peter Dillon <q12721@cusa.canon.com>
2019-01-08 10:08:53 -0500

    fake tcepps

--- activation-support/js/activation-support.js
+++ activation-support/js/activation-support.js
@@ -164,7 +164,7 @@ $(document).ready(function() {
         },
         { 
             className: "dt-center", 
-            targets: [12,13,22,23]
+            targets: [12,13,22]
         },
         { 
             targets: [0,1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17,18,19,20,21],
